85d357e1fe294858799cb075c5e0fa565ddac335
stevendev <stevendotpulido@gmail.com>
2018-12-01 19:07:44 -0800

    updated paths

--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-  <link rel="preload" as="font" href="/www.bonitabasics.dev/wp-content/themes/bonitabasics/assets/fonts/HelveticaNeue-CondensedBlack.woff2" type="font/woff2" crossorigin />
+  <link rel="preload" as="font" href="<?php bloginfo('url') ?>/wp-content/themes/bonitabasics/assets/fonts/HelveticaNeue-CondensedBlack.woff2" type="font/woff2" crossorigin />
   <?php wp_head(); ?>
 </head>
 
